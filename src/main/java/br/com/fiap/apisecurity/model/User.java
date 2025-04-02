c1005006ed9aad65c2143e898ef2a0a77189208a
fesilva2109 <felipesilvaa.2109@gmail.com>
2025-04-01 22:28:34 -0300

    Criando User Class

---
+++ src/main/java/br/com/fiap/apisecurity/model/User.java
@@ -0,0 +1,18 @@
+package br.com.fiap.apisecurity.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.UUID;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+        private UUID id;
+        private String username;
+        private String password;
+        private String role;
+}
