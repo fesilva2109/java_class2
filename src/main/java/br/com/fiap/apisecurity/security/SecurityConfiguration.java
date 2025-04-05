a5fb3cbfe66646c78c5a82c3ebd60c8e1c38005d
igor <igorsoosrribeiro@gmail.com>
2025-04-04 22:51:02 -0300

    Criando AuthDTO e Alterando AuthController e SecurityConfiguration

--- src/main/java/br/com/fiap/apisecurity/security/SecurityConfiguration.java
+++ src/main/java/br/com/fiap/apisecurity/security/SecurityConfiguration.java
@@ -2,10 +2,16 @@ package br.com.fiap.apisecurity.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
@@ -21,4 +27,18 @@ public class SecurityConfiguration {
                 )
                 .build();
     }
+    @Bean
+    public AuthenticationManager authenticationManager (AuthenticationConfiguration authenticationConfiguration)
+        throws Exception{
+            return authenticationConfiguration.getAuthenticationManager();
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder (){
+        return new BCryptPasswordEncoder();
+    }
+
+
+
 }
