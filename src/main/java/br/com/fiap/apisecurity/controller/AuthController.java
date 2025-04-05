a0cbe60eb0adffd16d2c97172c8ddd96eeba5b96
fesilva2109 <felipesilvaa.2109@gmail.com>
2025-04-04 23:03:47 -0300

    Criando RegisterDTO

--- src/main/java/br/com/fiap/apisecurity/controller/AuthController.java
+++ src/main/java/br/com/fiap/apisecurity/controller/AuthController.java
@@ -1,11 +1,14 @@
 package br.com.fiap.apisecurity.controller;
 
 import br.com.fiap.apisecurity.dto.AuthDTO;
+import br.com.fiap.apisecurity.dto.RegisterDTO;
+import br.com.fiap.apisecurity.model.User;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,6 +27,19 @@ public class AuthController {
                 authDTO.username(),
                 authDTO.password());
         var auth = this.authenticationManager.authenticate(userPwd);
+        return ResponseEntity.ok().build();
+    }
+    @PostMapping ("/register")
+    public ResponseEntity register(@RequestBody @Valid RegisterDTO registerDTO){
+        if(userRepository.findByUsername(registerDTO.username()) != null){
+            return ResponseEntity.badRequest().build();
+        }
+        String encryptedPwd = new BCryptPasswordEncoder()
+                .encode(registerDTO.password());
+        User newUser = new User(registerDTO.username(),
+                encryptedPwd,
+                registerDTO.role());
+        userRepository.save(newUser);
         return ResponseEntity.ok().build();
     }
 
